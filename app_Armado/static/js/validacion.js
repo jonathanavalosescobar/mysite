1066acdef3027564158a3a640bf6ca8a39778cf3
FelipeLucero <45726971+FelipeLucero@users.noreply.github.com>
2019-10-27 22:03:39 -0300

    Se arreglo validaciones js para registro de usuarios

--- app_Armado/static/js/validacion.js
+++ app_Armado/static/js/validacion.js
@@ -1,27 +1,20 @@
 function validacionesFormulario(){
     
-    var correo, rut, nombre,number,usuario,contraseña1,contraseña2
-    correo = document.getElementById("correo").value;
-    rut = document.getElementById("rut").value;
-    nombre = document.getElementById("nombre").value;
-    number = document.getElementById("number").value;
-    usuario = document.getElementById("usuario").value;
-    contraseña1 = document.getElementById("contraseña1").value;
-    contraseña2 = document.getElementById("contraseña2").value;
+    var nombres, apellidos, nombreUsuario,correo,contraseña1,contraseña2
+    nombres = document.getElementById("first_name").value;
+    apellidos = document.getElementById("last_name").value;
+    nombreUsuario = document.getElementById("username").value;
+    correo = document.getElementById("email").value;
+    contraseña1 = document.getElementById("password1").value;
+    contraseña2 = document.getElementById("password2").value;
     var espacios = false;
     var cont = 0;
    
 
-    if(correo == "" || rut == ""|| nombre == ""|| number == "" || usuario== ""|| contraseña1 == "" || contraseña2 == "" ){
+    if(nombres == "" || apellidos == ""|| nombreUsuario == ""|| correo == "" || contraseña1 == "" || contraseña2 == "" ){
         alert("Todos los campos son obligatorios ");
         return false;
     } 
-
-    if (isNaN(number)){
-        alert("El tipo de dato para telefono debe ser numerico");
-        return false;
-        
-    }
 
     while (!espacios && (cont < contraseña1.length)) {
       if (contraseña1.charAt(cont) == " ")
