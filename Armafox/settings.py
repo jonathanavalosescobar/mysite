db8fb0698aa001e7042f9f4824688a14d0d0c231
FelipeLucero <45726971+FelipeLucero@users.noreply.github.com>
2019-11-23 22:53:12 -0300

    se configura correo de envio

--- Armafox/settings.py
+++ Armafox/settings.py
@@ -122,10 +122,16 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ArmafoxDuocUC@GMAIL.COM' 
+EMAIL_HOST_PASSWORD = 'Caramelo1994 '
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/solicitud'
 LOGOUT_REDIRECT_URL = '/logged_out'
 
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+# EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
